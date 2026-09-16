11bbb47176181da795da6a6d92b26e78abca2341
Michael Bertram <141644516+michael-bertram@users.noreply.github.com>
2026-09-16 14:29:58 +0100

    feat: edit linked code examples through canonical entity

--- src/intelligent-code-assistant/index.js
+++ src/intelligent-code-assistant/index.js
@@ -1,38 +1,17 @@
 /**
- * Registers a new block provided a unique name and an object defining its behavior.
- *
- * @see https://developer.wordpress.org/block-editor/developers/block-api/#registering-a-block
+ * Registers the Intelligent Code Assistant block.
  */
 import { registerBlockType } from '@wordpress/blocks';
 
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Files imported from style.scss are bundled into the block's frontend stylesheet.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
 import './style.scss';
 import './assistant/style.scss';
 import './editor.scss';
 
-/**
- * Internal dependencies
- */
-import Edit from './edit';
+import EditRouter from './EditRouter';
 import Save from './save';
 import metadata from './block.json';
 
-import { InnerBlocks } from '@wordpress/block-editor';
-
-/**
- * Every block starts by registering a block type definition.
- *
- * @see ./edit.js
- */
 registerBlockType(metadata.name, {
-  /**
-   * @see ./edit.js
-   */
-  edit: Edit,
+  edit: EditRouter,
   save: Save,
 });
